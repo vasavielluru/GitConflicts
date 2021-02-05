04b4c54a51e98703e1f24fe8b2955aeabd90dfd5
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-05 08:00:19 +0530

    changes made locally

--- Git_Conflicts/src/Sum.java
+++ Git_Conflicts/src/Sum.java
@@ -4,9 +4,9 @@ public class Sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=10;
-		int b=15;
-		int sum=a+b;
+		int data1=10;
+		int data2=15;
+		int sum=data1+data2;
 		System.out.println(sum);
 
 	}
