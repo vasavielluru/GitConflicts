4b604d457d5f15813b4b3a19e48f8dcd837d61d2
vasavi elluru <elluruvasavi.19993@gmail.com>
2021-02-05 07:57:51 +0530

    commit msg

---
+++ Git_Conflicts/src/Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=15;
+		int sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
